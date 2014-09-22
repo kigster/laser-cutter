ec7f9e4a167e0b24acd9c515b637735099137b5e
Konstantin Gredeskoul <kigster@gmail.com>
2014-09-22 01:18:18 -0700

    Adding code climate to TravisCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,13 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
 require 'rubygems'
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 require 'laser-cutter'
+require "codeclimate-test-reporter"
 
-#Dir['spec/support/**/*.rb'].each { |filename| require_relative "../#{filename}" }
+
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
